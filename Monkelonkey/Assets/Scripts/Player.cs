82aca658bd92f513e6fa6f7a3885c7e9ee7f997a
NDalll <chr459@hotmail.com>
2022-02-28 23:12:33 +0100

    Pls virk

---
+++ Monkelonkey/Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+    public SpriteRenderer player;
+    public Rigidbody2D RB;
+    public float runAccel;
+    public float runDeccel;
+    public float velPower;
+    public float frictionAmount;
+    private PlayerControls playerControls;
+    public CapsuleCollider2D playerCollider;
+    public float coyoteTime;
+    private float lastGroundedTime = 0;
+    private float lastJumpedTime = 0;
+
+    [SerializeField] private LayerMask platformLayerMask;
+
+
+    private void FixedUpdate()
+    {
+        Vector2 input = playerControls.Default.Move.ReadValue<Vector2>();
+
+        if (input.x < 0)
+        {
+            player.flipX = true;
+        }
+        if (input.x > 0)
+        {
+            player.flipX = false;
+        }
+
+        float targetSpeed = input.x * moveSpeed; //calculate the direction we want to move in and our desired velocity
+        float speedDif = targetSpeed - RB.velocity.x; //calculate difference between current velocity and desired velocity
+        float accelRate;
+        
+        accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccel : runDeccel;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+        
+        RB.AddForce(movement * Vector2.right);
+
+        //Friction:
+        /*if (IsGrounded() && input.x == 0)
+        {
+            float amount = Mathf.Min(Mathf.Abs(RB.velocity.x), Mathf.Abs(frictionAmount));
+            amount *= Mathf.Sign(RB.velocity.x);
+            RB.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
+        }*/
+
+    }
+
+    private void Update()
+    {
+        //timer (for coyote time):
+        lastGroundedTime += Time.deltaTime;
+        lastJumpedTime += Time.deltaTime;
+
+        //jump
+        if ((IsGrounded()||(lastGroundedTime < coyoteTime && lastJumpedTime > coyoteTime * 2)) && playerControls.Default.Jump.triggered)
+        {
+            if (RB.velocity.y < 0) //Hvis spilleren falder vil hoppet stoppe dem
+            {
+                RB.velocity = new Vector2(RB.velocity.x, 0);
+            }
+            
+            RB.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            lastJumpedTime = 0;
+        }
+    }
+    private bool IsGrounded() //Check if player is on ground
+    {
+        
+        float extraHeightRaycast = 0.4f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size - new Vector3(0.1f, 0f, 0f), 0f, Vector2.down, extraHeightRaycast, platformLayerMask);
+        Color rayColor;
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+            lastGroundedTime = 0;
+
+        } else {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(playerCollider.bounds.center + new Vector3(playerCollider.bounds.extents.x, 0), Vector2.down * (playerCollider.bounds.extents.y + extraHeightRaycast), rayColor);
+        Debug.DrawRay(playerCollider.bounds.center - new Vector3(playerCollider.bounds.extents.x, 0), Vector2.down * (playerCollider.bounds.extents.y + extraHeightRaycast), rayColor);
+        Debug.DrawRay(playerCollider.bounds.center - new Vector3(playerCollider.bounds.extents.x, playerCollider.bounds.extents.y + extraHeightRaycast), Vector2.right * (playerCollider.bounds.extents.x), rayColor);
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
+    }
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+}
