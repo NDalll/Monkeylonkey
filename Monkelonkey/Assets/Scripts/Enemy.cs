c987b4f4b6adfeffec757ddd35e1644f868f2118
NDalll <chr459@hotmail.com>
2022-03-16 16:39:36 +0100

    folder stuff

---
+++ Monkelonkey/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+    }
+
+
+}
