1c0c3458cdf4c37a7683caf6e2e4547e5c15fd84
NDalll <chr459@hotmail.com>
2022-04-13 18:34:06 +0200

    hbjik

--- Monkelonkey/Assets/TutorialClear.cs
+++ Monkelonkey/Assets/TutorialClear.cs
@@ -6,22 +6,30 @@ using UnityEngine.SceneManagement;
 public class TutorialClear : MonoBehaviour
 {
     private Gamecontroller gamecontroller;
+    public bool isTutorial;
     void Start()
     {
         gamecontroller = GameObject.FindGameObjectWithTag("Gamecontroller").GetComponent<Gamecontroller>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       
-            if (collision.CompareTag("Player"))
+
+        if (collision.CompareTag("Player"))
+        {
+            if (isTutorial)
             {
-           
                 gamecontroller.timePlayed = 0;
                 gamecontroller.enemiesDefeated = 0;
                 gamecontroller.bananas = 0;
                 gamecontroller.bananasCollected = 0;
                 gamecontroller.floorsBeaten = 0;
                 SceneManager.LoadScene("Gameplay");
+            }
+            else
+            {
+                SceneManager.LoadScene("Gameover");
+            } 
         }
+        
     }
 }
