438627322d7bfa58099f75981091afedc3217ded
NDalll <chr459@hotmail.com>
2022-04-16 00:41:56 +0200

    3-opening-rooms done

--- Monkelonkey/Assets/Scripts/MeleeWeapon.cs
+++ Monkelonkey/Assets/Scripts/MeleeWeapon.cs
@@ -7,7 +7,10 @@ public class MeleeWeapon : MonoBehaviour
     // Start is called before the first frame update
     [System.NonSerialized]
     public float damage;
-
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")){
