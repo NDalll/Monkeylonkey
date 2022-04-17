2a542fe3b939797692b517d3e4e5f069e4e8e81a
NDalll <chr459@hotmail.com>
2022-04-18 01:29:23 +0200

    ups

--- Monkelonkey/Assets/Scripts/WallTrigger.cs
+++ Monkelonkey/Assets/Scripts/WallTrigger.cs
@@ -2,34 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallTrigger : MonoBehaviour
+public class WallTrigger : MonoBehaviour //dette er scriptet på collideren foran fjenden
 {
     private Enemy entity;
     // Start is called before the first frame update
     void Start()
     {
-        entity = transform.parent.GetComponent<Enemy>();
+        entity = transform.parent.GetComponent<Enemy>(); //reference til fjenden
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision) //kaldes ved collision
     {
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ground"))
+        if (collision.CompareTag("Ground")) //hvis det er jorden
         {
-            entity.LedgeJump();
+            entity.LedgeJump();//kalder fjendens ledgejump funktion
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)//kaldes når den forlader jorden
     {
         if (collision.CompareTag("Ground"))
         {
-            entity.runSpeed = entity.ogRunSpeed;
-            entity.walkSpeed = entity.ogWalkSpeed;
-            entity.RB.velocity = new Vector2 (entity.RB.velocity.x, 0.2f);
+            entity.runSpeed = entity.ogRunSpeed; //sætter dens hastighed til det originale (bliver sat til 0 når den hopper)
+            entity.walkSpeed = entity.ogWalkSpeed; //sætter dens hastighed til det originale (bliver sat til 0 når den hopper)
+            entity.RB.velocity = new Vector2 (entity.RB.velocity.x, 0.2f);//stopper dens hastighed opad (derfor hopper den kun præcist så højt den har brug for)
         }
     }
 }
